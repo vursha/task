0e658be146ff8c23f3b9a18e63ce249a9b633341
vursha <97270930+vursha@users.noreply.github.com>
2022-01-17 21:19:24 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import React from "react";
-import  ReactDOM from 'react-dom';
-import './App.css';
- const {useState} = React;
-function App(){
-  const[count,setCount] = useState(0);
-  return(
-    <div className="app">
-      <h1 className={count>5 ? "beforeFive" :count<5 ? "afterFive":null}> {count}</h1>
-      <div className="button">
-        <button onClick={() =>setCount(count-1)}>-</button>
-       
-        <button onClick={() =>setCount(count+1)}>+</button>
-    
+import React from 'react';
+import './index.css';
+
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: [],
+    };
+    console.log('hi...');
+  }
+  componentDidMount() {
+    console.log('hi123');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          userData: json,
+        });
+      });
+    console.log(this.state.userData);
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1> Fetch data from an api in react </h1>{' '}
+        {this.state.userData.map((item) => (
+         <ol key={item.id}>
+            <img src='./images/nature images/butterfly.jpg' alt=""/>
+            <li> Name:{item.username}</li>
+            <li> Email:{item.email}</li>
+           
+          </ol>
+        ))}
       </div>
-    </div>
-  )
+    );
+  }
 }
-ReactDOM.render(<App/>,document.getElementById("root"));
-export default App;
